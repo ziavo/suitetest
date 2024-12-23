399d1a9223ee26e7654b84a7d3af5a117f5948ae
ziavo <58455767+ziavo@users.noreply.github.com>
2024-12-23 01:49:13 -0500

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,4 @@
+local rbxmSuite = loadstring(game:HttpGetAsync("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))()
+
+local path = rbxmSuite.download("ziavo/suitetest@latest", "suitetest.rbxm")
+local model = rbxmSuite.launch(path)
